0e71b523ccc9365538e95e3a0e73ff1cc9747a94
its-edalo <itsedalo@gmail.com>
2025-04-07 00:16:55 +0300

    Add a check that area.cards is not nil (#605)

--- src/utils.lua
+++ src/utils.lua
@@ -344,9 +344,11 @@ function SMODS.find_card(key, count_debuffed)
     local results = {}
     if not G.jokers or not G.jokers.cards then return {} end
     for _, area in ipairs(SMODS.get_card_areas('jokers')) do
-        for _, v in pairs(area.cards) do
-            if v and type(v) == 'table' and v.config.center.key == key and (count_debuffed or not v.debuff) then
-                table.insert(results, v)
+        if area.cards then
+            for _, v in pairs(area.cards) do
+                if v and type(v) == 'table' and v.config.center.key == key and (count_debuffed or not v.debuff) then
+                    table.insert(results, v)
+                end
             end
         end
     end
