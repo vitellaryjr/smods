16c175f960c6656117237067cf2c07786c3f0ae8
Eremel <richardbailey@outlook.com>
2025-04-10 22:50:19 +0100

    Add 'balance' calculation return key

--- src/utils.lua
+++ src/utils.lua
@@ -1274,6 +1274,59 @@ SMODS.calculate_individual_effect = function(effect, scored_card, key, amount, f
         mult = mod_mult(hand_chips)
         hand_chips = mod_chips(old_mult)
         update_hand_text({delay = 0}, {chips = hand_chips, mult = mult})
+        scored_card:juice_up()
+        return true
+    end
+
+    if key == 'balance' then
+        if effect.card and effect.card ~= scored_card then juice_card(effect.card) end
+        local total = mult + hand_chips
+        mult = mod_mult(total/2)
+        hand_chips = mod_chips(total/2)
+        update_hand_text({delay = 0}, {chips = hand_chips, mult = mult})
+        G.E_MANAGER:add_event(Event({
+            func = (function()
+                -- scored_card:juice_up()
+                play_sound('gong', 0.94, 0.3)
+                play_sound('gong', 0.94*1.5, 0.2)
+                play_sound('tarot1', 1.5)
+                ease_colour(G.C.UI_CHIPS, {0.8, 0.45, 0.85, 1})
+                ease_colour(G.C.UI_MULT, {0.8, 0.45, 0.85, 1})
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'after',
+                    blockable = false,
+                    blocking = false,
+                    delay =  0.8,
+                    func = (function() 
+                            ease_colour(G.C.UI_CHIPS, G.C.BLUE, 0.8)
+                            ease_colour(G.C.UI_MULT, G.C.RED, 0.8)
+                        return true
+                    end)
+                }))
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'after',
+                    blockable = false,
+                    blocking = false,
+                    no_delete = true,
+                    delay =  1.3,
+                    func = (function() 
+                        G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] = G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
+                        G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] = G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
+                        return true
+                    end)
+                }))
+                return true
+            end)
+        }))
+        if not effect.remove_default_message then
+            if effect.balance_message then
+                card_eval_status_text(effect.message_card or effect.juice_card or scored_card or effect.card or effect.focus, 'extra', nil, percent, nil, effect.xmult_message)
+            else
+                card_eval_status_text(effect.message_card or effect.juice_card or scored_card or effect.card or effect.focus, 'extra', nil, percent, nil, {message = localize('k_balanced'), colour =  {0.8, 0.45, 0.85, 1}})
+            end
+        end
+        delay(0.6)
+
         return true
     end
 
@@ -1354,7 +1407,7 @@ SMODS.calculation_keys = {
     'x_chips', 'xchips', 'Xchip_mod',
     'x_mult', 'Xmult', 'xmult', 'x_mult_mod', 'Xmult_mod',
     'p_dollars', 'dollars', 'h_dollars',
-    'swap',
+    'swap', 'balance',
     'saved', 'effect', 'remove',
     'debuff', 'prevent_debuff', 'debuff_text',
     'add_to_hand', 'remove_from_hand',
