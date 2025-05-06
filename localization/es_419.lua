c9297f620b131d44810e38c656d6afd3ef210065
N <venturinosebastian@gmail.com>
2025-05-05 22:36:14 -0300

    Update es_ES and es_419 localizations (#676)

--- localization/es_419.lua
+++ localization/es_419.lua
@@ -62,13 +62,107 @@ return {
                     '¡Este mod ha sido',
                     '{C:attention}desactivado{}!'
                 }
-            }
+            },
+            -- card perma bonuses
+            card_extra_chips = {
+                text = {
+                    "{C:chips}#1#{} fichas extra",
+                },
+            },
+            card_x_chips = {
+                text = {
+                    "{X:chips,C:white}X#1#{} fichas"
+                }
+            },
+            card_extra_x_chips = {
+                text = {
+                    "{X:chips,C:white}X#1#{} fichas extra"
+                }
+            },
+            card_extra_mult = {
+                text = {
+                    "{C:mult}#1#{} multi extra"
+                }
+            },
+            card_x_mult = {
+                text = {
+                    "{X:mult,C:white}X#1#{} multi"
+                }
+            },
+            card_extra_x_mult = {
+                text = {
+                    "{X:mult,C:white}X#1#{} multi extra"
+                }
+            },
+            card_extra_p_dollars = {
+                text = {
+                    "{C:money}#1#{} cuando anota",
+                }
+            },
+            card_extra_h_chips = {
+                text = {
+                    "{C:chips}#1#{} fichas mientras esté en tu mano",
+                }
+            },
+            card_h_x_chips = {
+                text = {
+                    "{X:chips,C:white}X#1#{} fichas mientras esté en tu mano",
+                }
+            },
+            card_extra_h_x_chips = {
+                text = {
+                    "{X:chips,C:white}X#1#{} fichas extra mientras esté en tu mano",
+                }
+            },
+            card_extra_h_mult = {
+                text = {
+                    "{C:mult}#1#{} multi extra mientras esté en tu mano",
+                }
+            },
+            card_h_x_mult = {
+                text = {
+                    "{X:mult,C:white}X#1#{} multi mientras esté en tu mano",
+                }
+            },
+            card_extra_h_x_mult = {
+                text = {
+                    "{X:mult,C:white}X#1#{} multi extra mientras esté en tu mano",
+                }
+            },
+            card_extra_h_dollars = {
+                text = {
+                    "{C:money}#1#{} si está en tu mano al final de la ronda",
+                },
+            },
         },
         Edition = {
             e_negative_playing_card = {
                 name = "Negativa",
                 text = {
                     "{C:dark_edition}+#1#{} de tamaño de mano"
+                },
+            },
+        },
+        Enhanced = {
+            m_gold = {
+                name = "Carta de oro",
+                text = {
+                    "{C:money}#1#{} si esta",
+                    "carta está en tu mano",
+                    "al final de la ronda",
+                },
+            },
+            m_stone = {
+                name = "Carta de piedra",
+                text = {
+                    "{C:chips}#1#{} fichas",
+                    "sin categoría ni palo",
+                },
+            },
+            m_mult = {
+                name = "Carta multi",
+                text = {
+                    "{C:mult}#1#{} multi",
                 },
             },
         }
@@ -108,10 +202,33 @@ return {
             b_applies_stakes_1 = 'Aplica ',
             b_applies_stakes_2 = '',
             b_graphics_mipmap_level = "Nivel de Mipmap",
+            b_browse = 'Navegar',
+            b_search_prompt = 'Buscar mods',
+            b_search_button = 'Buscar',
+            b_seeded_unlocks = 'Desbloqueos con código',
+            b_seeded_unlocks_info = 'Habilita desbloqueos y descubrimientos en partidas con código',
+            ml_achievement_settings = {
+                'Deshabilitado',
+                'Habilitado',
+                'Ignorar Restricciones'
+            },
+            b_deckskins_lc = 'Colores de bajo contraste',
+            b_deckskins_hc = 'Colores de alto contraste',
+            b_deckskins_def = 'Colores por defecto',
         },
         v_dictionary = {
             c_types = '#1# Tipos',
             cashout_hidden = '...y #1# más',
+            a_xchips = "X#1# fichas",
+            a_xchips_minus = "-X#1# fichas",
+            smods_version_mismatch = {
+                "¡Tu versión de Steamodded ha cambiado",
+                "desde que has comenzado esta partida!",
+                "Continuarla podría producir",
+                "comportamiento inesperado y que el juego se bloquee.",
+                "Versión de inicio: #1#",
+                "Versión actual: #2#",
+            }
         },
     }
 }
