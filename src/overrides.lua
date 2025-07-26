c0fda63b7b902827a4c7df6d56d15125f909e407
BepisFever <nguyendoangiabao220808@gmail.com>
2025-07-27 04:16:27 +0700

    Added context.change_suit and context.change_rank (#813)

--- src/overrides.lua
+++ src/overrides.lua
@@ -2311,6 +2311,27 @@ function Blind:modify_hand(cards, poker_hands, text, mult, hand_chips, scoring_h
 	return _G.mult, _G.hand_chips, modded or flags.calculated
 end
 
+local card_set_base = Card.set_base
+function Card:set_base(card, initial)
+    if self.playing_card and self.base then
+        local new_rank = card and card.value and SMODS.Ranks[card.value] and SMODS.Ranks[card.value].id
+        local contexts = {}
+        if new_rank then
+            if self.base.id and self.base.id ~= new_rank then
+                SMODS.merge_defaults(contexts, {change_rank = true, other_card = self, new_rank = new_rank, old_rank = self.base.id, rank_increase = ((self.base.id < new_rank) and true) or false})
+            end
+        end
+        if card and card.suit and self.base.suit ~= card.suit then 
+            SMODS.merge_defaults(contexts, {change_suit = true, other_card = self, new_suit = card.suit, old_suit = self.base.suit})
+        end
+        if next(contexts) then
+            SMODS.calculate_context(contexts)
+        end
+    end
+
+    card_set_base(self, card, initial)
+end
+
 local use_consumeable = Card.use_consumeable
 function Card:use_consumeable(area, copier)
 	local ret = use_consumeable(self, area, copier)
