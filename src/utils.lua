59aa94303e96ac14b594a4a1ca0fb7149cbc9c7f
Osquo <85627730+JustOsquo@users.noreply.github.com>
2025-08-08 01:58:23 +0200

    Fix smart_level_up_hand() not properly updating hand text for hands with face-down cards (#800)

--- src/utils.lua
+++ src/utils.lua
@@ -1213,7 +1213,11 @@ SMODS.smart_level_up_hand = function(card, hand, instant, amount)
     local vals_after_level
     if SMODS.displaying_scoring and not (SMODS.displayed_hand == hand) then
         vals_after_level = copy_table(G.GAME.current_round.current_hand)
-        vals_after_level.level = (G.GAME.hands[vals_after_level.handname] or {}).level or ''
+        local text,disp_text,_,_,_ = G.FUNCS.get_poker_hand_info(G.play.cards)
+        vals_after_level.handname = disp_text or ''
+        vals_after_level.level = (G.GAME.hands[text] or {}).level or ''
+        vals_after_level.chips = number_format(hand_chips) or 0
+        vals_after_level.mult = number_format(mult) or 0
     end
     if not (instant or SMODS.displayed_hand == hand) then
         update_hand_text({sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3}, {handname=localize(hand, 'poker_hands'),chips = G.GAME.hands[hand].chips, mult = G.GAME.hands[hand].mult, level=G.GAME.hands[hand].level})
