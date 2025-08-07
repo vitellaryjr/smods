80c528bc785f51b960687d55e5374fbc0270cd3f
Eremel <richardbailey@outlook.com>
2025-08-07 01:03:01 +0100

    Stickers now automatically copy their `config` table

--- src/game_object.lua
+++ src/game_object.lua
@@ -2917,6 +2917,16 @@ Set `prefix_config.key = false` on your object instead.]]):format(obj.key), obj.
         end,
         apply = function(self, card, val)
             card.ability[self.key] = val
+            if val and next(self.config) then
+                card.ability[self.key] = {}
+                for k, v in pairs(self.config) do
+                    if type(v) == 'table' then
+                        card.ability[self.key][k] = copy_table(v)
+                    else
+                        card.ability[self.key][k] = v
+                    end
+                end
+            end
         end
     }
 
