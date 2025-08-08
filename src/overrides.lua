cf5ea1a603abec52160ffab8a34cdce7ae487aa0
AllUniversal <114624150+AllUniversal@users.noreply.github.com>
2025-08-09 01:27:57 +0200

    Negative playing cards draw fix (#854)

--- src/overrides.lua
+++ src/overrides.lua
@@ -1869,8 +1869,8 @@ function Card:set_edition(edition, immediate, silent, delay)
 							G.hand.config.real_card_limit = G.hand.config.real_card_limit + self.edition.card_limit
 						end
 						G.hand.config.card_limit = G.hand.config.card_limit + self.edition.card_limit
-						if not is_in_pack and G.GAME.blind.in_blind then
-							G.FUNCS.draw_from_deck_to_hand(self.edition.card_limit)
+						if not is_in_pack and G.GAME.blind.in_blind and G.hand.config.card_limit > #G.hand.cards then
+							G.FUNCS.draw_from_deck_to_hand(math.min(self.edition.card_limit, G.hand.config.card_limit - #G.hand.cards))
 						end
 						return true
 					end
