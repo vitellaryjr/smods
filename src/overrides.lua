3ad17b8287f95928aeb8c4ad2a39b367205b664f
Vivian Giacobbi <57081585+VivianGiacobbi@users.noreply.github.com>
2025-08-08 18:34:58 -0500

    Fixed a regression (#869)

--- src/overrides.lua
+++ src/overrides.lua
@@ -2312,7 +2312,7 @@ function Blind:modify_hand(cards, poker_hands, text, mult, hand_chips, scoring_h
 end
 
 local card_set_base = Card.set_base
-function Card:set_base(card, initial)
+function Card:set_base(card, initial, manual_sprites)
     if self.playing_card and self.base then
         local new_rank = card and card.value and SMODS.Ranks[card.value] and SMODS.Ranks[card.value].id
         local contexts = {}
@@ -2329,7 +2329,7 @@ function Card:set_base(card, initial)
         end
     end
 
-    card_set_base(self, card, initial)
+    card_set_base(self, card, initial, manual_sprites)
 end
 
 local use_consumeable = Card.use_consumeable
