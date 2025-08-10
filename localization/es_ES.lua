069719c510aa9108df3168fedc27d96330095655
N <venturinosebastian@gmail.com>
2025-08-10 20:42:46 -0300

    Update `es_419.lua` and `es_ES.lua` (#815)

--- localization/es_ES.lua
+++ localization/es_ES.lua
@@ -134,6 +134,23 @@ return {
                     "{C:money}#1#{} si está en tu mano al final de la ronda",
                 },
             },
+            card_extra_repetitions = {
+                text = {
+                    "Reactiva esta",
+                    "carta {C:attention}#1#{} #2#",
+                },
+            },
+            artist = {
+                text = {
+                    "{C:inactive}Artista",
+                },
+            },
+            artist_credit = {
+                name = "Artista",
+                text = {
+                    "{E:1}#1#{}"
+                },
+            },
         },
         Edition = {
             e_negative_playing_card = {
@@ -163,6 +180,15 @@ return {
                 name = "Carta multi",
                 text = {
                     "{C:mult}#1#{} multi",
+                },
+            },
+            m_lucky = {
+                name = "Carta de la suerte",
+                text = {
+                    "{C:green}#1# en #3#{} probabilidades",
+                    "de {C:mult}+#2#{} multi",
+                    "{C:green}#6# en #5#{} probabilidades",
+                    "de ganar {C:money}$#4#",
                 },
             },
         }
@@ -195,6 +221,7 @@ return {
             b_unknown = 'Desconocido',
             b_lovely_mod = '(Mod de Lovely) ',
             b_by = ' Por: ',
+            b_priority = 'Prioridad: ',
             b_config = "Configuración",
             b_additions = 'Adiciones',
             b_stickers = 'Stickers',
@@ -215,6 +242,9 @@ return {
             b_deckskins_lc = 'Colores de bajo contraste',
             b_deckskins_hc = 'Colores de alto contraste',
             b_deckskins_def = 'Colores por defecto',
+            b_limit = 'Hasta ',
+            b_retrigger_single = 'vez',
+            b_retrigger_plural = 'veces'
         },
         v_dictionary = {
             c_types = '#1# Tipos',
