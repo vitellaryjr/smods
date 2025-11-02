4f8f17e0580720bc440796184f40b645e667c82b
Sylvi <69098783+wingedcatgirl@users.noreply.github.com>
2025-11-01 23:35:11 -0400

    Update sticker.lua (#1061)
    Tyop in a field

--- lsp_def/classes/sticker.lua
+++ lsp_def/classes/sticker.lua
@@ -13,7 +13,7 @@
 ---@field default_compat? boolean Default compatibility with cards. 
 ---@field compat_exceptions? string[] Array of keys to centers that are exceptions to `default_compat`. 
 ---@field sets? string[] Array of keys to pools that this sticker is allowed to be applied on. 
----@field needs_enabled_flag? boolean Sets whether the sticker requires `G.GAME.modifiers["enable_"..key]` to be `true` before it can be applied. 
+---@field needs_enable_flag? boolean Sets whether the sticker requires `G.GAME.modifiers["enable_"..key]` to be `true` before it can be applied. 
 ---@field sticker_sprite? Sprite|table Sprite object of the sticker. 
 ---@field __call? fun(self: SMODS.Sticker|table, o: SMODS.Sticker|table): nil|table|SMODS.Sticker
 ---@field extend? fun(self: SMODS.Sticker|table, o: SMODS.Sticker|table): table Primary method of creating a class. 
