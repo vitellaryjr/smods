3049973ca166cf2bb5a4d7833ef35fb02713d24e
Victor Oleksiienko <83234016+stupxd@users.noreply.github.com>
2026-03-07 18:02:45 +0200

    Support starting & custom modifiers without UI for Challenges (#1230)

--- lsp_def/classes/challenge.lua
+++ lsp_def/classes/challenge.lua
@@ -4,7 +4,7 @@
 ---@field obj_table? table<string, SMODS.Challenge|table> Table of objects registered to this class.
 ---@field loc_txt? table|{name: string} Contains strings used for displaying text related to this object.
 ---@field super? SMODS.GameObject|table Parent class.
----@field rules? table|{custom?: table[]|{id: string, value?: any}[], modifiers?: table[]|{id: string, value?: any}} List of tables setting custom rules and modifiers. Each rule/modifier has an `id`, with optional `value` param (defaults to `true`). See [SMODS.Challenge](https://github.com/Steamodded/smods/wiki/SMODS.Challenge) Documentation for default rules/modifiers.
+---@field rules? table|{custom?: table[]|{id: string, value?: any, no_ui?: boolean}[], modifiers?: table[]|{id: string, value?: any, no_ui?: boolean}} List of tables setting custom rules and modifiers. Each rule/modifier has an `id`, with optional `value` param (defaults to `true`) and optional `no_ui` parameter (not set by default). See [SMODS.Challenge](https://github.com/Steamodded/smods/wiki/SMODS.Challenge) Documentation for default rules/modifiers.
 ---@field jokers? table[]|{id: string, edition?: string, eternal?: boolean, pinned?: boolean}[] List of tables setting jokers to start with.
 ---@field consumeables? table[]|{id: string, edition?: string, eternal?: boolean}[] List of tables setting consumables to start with.
 ---@field vouchers? table[]|{id: string}[] List of tables setting vouchers to start with.
