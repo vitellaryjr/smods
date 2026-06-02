8f3742707c654c61cc08aa7bfbdf09c696de9e55
NeatsTopFoo <143466477+NeatsTopFoo@users.noreply.github.com>
2026-06-02 03:24:13 +0100

    Feat: Add ``Card:can_sell()`` API method (#1254)
    * Add Card:can_sell() API method
    
    Identical to other API method patches - But from what I can see, looks like it needs to be in better_calc.toml? As the area where the API method needs to be directly follows an eternal check and the next thing after that is a return.
    
    May need to be moved to center.toml
    
    * Fix ``can_sell()`` API method
    
    Ret check changed to a more strict nil check as it was ignoring falsy values
    
    * docs: lsp def for can_sell
    
    * docs: clarification
    
    * accidentally deleted card without noticing oops
    
    * Remove 2nd return value & Simplify

--- lsp_def/classes/consumable.lua
+++ lsp_def/classes/consumable.lua
@@ -27,6 +27,7 @@
 ---@field keep_on_use? fun(self: SMODS.Consumable|table, card: Card|table): boolean? Return `true` if the consumable should stay after use.
 ---@field calc_dollar_bonus? fun(self: SMODS.Consumable|table, card: Card|table): number?, {text?:string, text_colour?:table, scale?:number, key?:string, set?:string}? Calculates reward money. 
 ---@field calc_scaling? fun(self: SMODS.Consumable|table, card: Card|table, other_card: Card|table, scaling_value: number, scalar_value: number, args: table): table? Called by `SMODS.scale_card`. Allows detection and modification of cards when scaling values. The return may include a `scaling_value` or `scalar_value` field to modify those values or any standard calculation return.
+---@field can_sell? fun(self: SMODS.Consumable|table, card: Card|table, context: unknown?):boolean? Allows configuring if the card is allowed to be sold. (Context is not used in vanilla and it's not the same as `calculate`)
 ---@overload fun(self: SMODS.Consumable): SMODS.Consumable
 SMODS.Consumable = setmetatable({}, {
     __call = function(self)
