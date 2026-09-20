1ba498b6e49cb2fef0937ba249037d2ed39471c6
Eremel <richardbailey@outlook.com>
2026-09-20 21:33:35 +0100

    More stake order fixes

--- src/game_object.lua
+++ src/game_object.lua
@@ -825,16 +825,22 @@ Set `prefix_config.key = false` on your object instead.]]):format(obj.key), obj.
         end,
         post_inject_class = function(self)
             -- sort stakes into the correct spot
-            table.sort(G.P_CENTER_POOLS[self.set], function(a, b) return a.order > b.order end)
-            for i, v in ipairs(G.P_CENTER_POOLS[self.set]) do
-                if v.above_stake and G.P_STAKES[v.above_stake] and v.order ~= G.P_STAKES[v.above_stake].order + 1 then
-                    local new_order = G.P_STAKES[v.above_stake].order + 1
-                    v.order = new_order
-                    for _, stake in pairs(G.P_STAKES) do
-                        if stake ~= v and stake.order >= new_order then stake.order = stake.order + 1 end
+            local sorted = false
+            while not sorted do
+                sorted = true
+                table.sort(G.P_CENTER_POOLS[self.set], function(a, b) return a.order > b.order end)
+                for i, v in ipairs(G.P_CENTER_POOLS[self.set]) do
+                    if v.above_stake and G.P_STAKES[v.above_stake] and v.order ~= G.P_STAKES[v.above_stake].order + 1 then
+                        sorted = false
+                        local new_order = G.P_STAKES[v.above_stake].order + 1
+                        v.order = new_order
+                        for _, stake in pairs(G.P_STAKES) do
+                            if stake ~= v and stake.order >= new_order then stake.order = stake.order + 1 end
+                        end
                     end
                 end
             end
+            
             -- until stakes_fixed
             table.sort(G.P_CENTER_POOLS[self.set], function(a, b) return a.order < b.order end)
             for i,v in ipairs(G.P_CENTER_POOLS[self.set]) do
--- version.lua
+++ version.lua
@@ -1,1 +1,1 @@
-return "26.920.0~dev-a"
+return "26.920.0~dev-b"
