809612a22626f891f0eecf478fce13c202f6c0fc
N <venturinosebastian@gmail.com>
2026-09-23 13:59:09 -0300

    remove print

--- src/game_object.lua
+++ src/game_object.lua
@@ -844,7 +844,6 @@ Set `prefix_config.key = false` on your object instead.]]):format(obj.key), obj.
             -- until stakes_fixed
             table.sort(G.P_CENTER_POOLS[self.set], function(a, b) return a.order < b.order end)
             for i,v in ipairs(G.P_CENTER_POOLS[self.set]) do
-                print(v.key, i)
                 G.P_STAKES[v.key].order = i
             end
             for _,stake in pairs(G.P_CENTER_POOLS.Stake) do
